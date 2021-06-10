f620f629534d491efaafb8352abaf60a942bf3ac
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-10 19:22:11 +0900

    Changed the method

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,70 @@
 import * as THREE from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { VRM, VRMSchema } from '@pixiv/three-vrm'
 
 window.addEventListener("DOMContentLoaded", ()=> {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.set(0,1,3);
+// レンダラーの設定
+  var  canvas = <HTMLCanvasElement> document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({
+  canvas: <HTMLCanvasElement> document.querySelector('#canvas'),
+  antialias: true,
+  alpha: true,
+})
+renderer.setSize(canvas.clientWidth, canvas.clientHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement)
 
-  const renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x000000);
-  document.body.appendChild(renderer.domElement);
+// カメラの設定
+const camera = new THREE.PerspectiveCamera(
+  35,
+  canvas.clientWidth / canvas.clientHeight,
+  0.1,
+  1000,
+)
+camera.position.set(0, 1, 4)
 
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(1,1,1).normalize();
-  scene.add(light);
+// カメラコントーロールの設定
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.target.set(0, 0.85, 0)
+controls.screenSpacePanning = true
+controls.update()
 
-  const loader = new GLTFLoader();
-  loader.load(
-    './static/models/test.vrm',
+// シーンの設定
+const scene = new THREE.Scene()
 
+// ライトの設定
+const light = new THREE.DirectionalLight(0xffffff)
+light.position.set(1, 1, 1).normalize()
+scene.add(light)
+
+// グリッドを表示
+const gridHelper = new THREE.GridHelper(10, 10)
+scene.add(gridHelper)
+gridHelper.visible = true
+
+// 座標軸を表示
+const axesHelper = new THREE.AxesHelper(0.5)
+  scene.add(axesHelper)
+
+// VRMの読み込み
+  const loader = new GLTFLoader()
+  loader.load('./static/models/test.vrm',
     (gltf) => {
       VRM.from(gltf).then( (vrm) => {
-        scene.add(vrm.scene);
-        vrm.scene.rotation.y = Math.PI;
-
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm)!.rotation.x = 0.6;
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftLowerArm)!.rotation.x = 0.8;
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftLowerArm)!.rotation.y = -1.;
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftHand)!.rotation.y = -0.5;
-        vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm)!.rotation.z = -1.3;
-
-        vrm.blendShapeProxy!.setValue(VRMSchema.BlendShapePresetName.Fun, .7);
-        vrm.blendShapeProxy!.setValue(VRMSchema.BlendShapePresetName.Sorrow, .2);
-        vrm.blendShapeProxy!.update();
-
+        // シーンへの追加
+        scene.add(vrm.scene)
+        vrm.scene.rotation.y = Math.PI
       })
     }
   )
 
-  const update = () => {
-    requestAnimationFrame(update);
-    renderer.render(scene, camera);
-  };
-  update();
+// 初回実行
+tick()
+
+function tick() {
+  requestAnimationFrame(tick)
+  // レンダリング
+  renderer.render(scene, camera)
+}
 })
