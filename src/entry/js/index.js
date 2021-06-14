95e3c94479c87f56bbe6848e1d04fcf5627e4cc6
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-15 02:37:28 +0900

    change voice recognition to another file

--- src/entry/js/index.js
+++ src/entry/js/index.js
@@ -3,6 +3,7 @@ import '../index.ts';
 
 // JavaScript
 import '../js/loading.js';
+import '../js/speechRecognition.js';
 
 // Sass
 import '../style/index.scss';
---
+++ src/entry/js/speechRecognition.js
@@ -0,0 +1,28 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'ja';
+recognition.continuous = true;
+recognition.interimResults = true;
+
+let finalTranscript = '';
+
+document.getElementById('recStart_').onclick = function recStart() {
+  recognition.start();
+}
+document.getElementById('recStop_').onclick = function recStop() {
+  recognition.stop();
+}
+
+recognition.onresult = function (event) {
+    let interimTranscript = '';
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+            transcript += "。\n";
+            finalTranscript += transcript;
+        } else {
+            interimTranscript = transcript;
+        }
+    }
+    document.getElementById("chatMessage").value = finalTranscript + interimTranscript;
+}
