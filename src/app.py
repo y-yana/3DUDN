e77d9e31ca3f73feff33cb7354b2c1a4e416681d
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-14 20:37:30 +0900

    ajax test success

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template, jsonify, request
+import json
 app = Flask(__name__)
 
 
@@ -7,7 +7,14 @@ app = Flask(__name__)
 def index():
     #return "Hello World"
     return render_template("index.html")
-  
+
+# /showにPOSTリクエストが送られたら処理してJSONを返す
+@app.route('/show', methods=['POST'])
+def show():
+    return_json = {
+        "message": f"Hello, {request.form['username']}"
+    }
+    return jsonify(values=json.dumps(return_json))
 
 if __name__ == '__main__':
   app.run()
