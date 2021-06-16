471b6a79f11c2bb5abbcedfde3adc71498098b86
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-16 20:41:52 +0900

    delete comment out

--- src/entry/index.ts
+++ src/entry/index.ts
@@ -1,4 +1,3 @@
-//declare var require: any;
 import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
