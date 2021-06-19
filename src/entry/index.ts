63498043c1fbd71f2d0687bba7feafd26e45bc3e
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-19 22:53:01 +0900

    canvas responsive success!!

--- src/entry/index.ts
+++ src/entry/index.ts
@@ -6,13 +6,24 @@ import { updateArrayBindingPattern } from 'typescript';
 
 window.addEventListener("DOMContentLoaded", () => {
   // canvasサイズの制御
+  // 表示用のサイズを格納する変数
+  var newWidth
+  var newHeight
+
   // 画面サイズを取得
   var getWidth = window.innerWidth;
   var getHeight = window.innerHeight;
 
-  // 比率計算
-  var newWidth = Math.floor(getWidth * (2 / 5))
-  var newHeight = Math.floor(getHeight * (4 / 5))
+  // レスポンシブ対応
+  if (getWidth <= 950) {
+    // 比率計算(mobile)
+    newWidth = Math.floor(getWidth * 0.8)
+    newHeight = Math.floor(getHeight * 0.8)
+  } else {
+    // 比率計算(desktop)
+    newWidth = Math.floor(getWidth * (2 / 5))
+    newHeight = Math.floor(getHeight * (4 / 5))
+  }
 
   // canvas生成
   var modelArea = document.getElementById('modelArea');
