171150a376fdd5a8554919c8aaf901615476e1bd
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-06-20 14:09:35 +0900

    camera control is not possible mobile

--- src/entry/index.ts
+++ src/entry/index.ts
@@ -75,12 +75,15 @@ window.addEventListener("DOMContentLoaded", () => {
     1000,
   )
   camera.position.set(0, 1, 4)
- 
+
+    // レスポンシブ対応
+  if (getWidth > 950) {
   // カメラコントーロールの設定
   const controls = new OrbitControls(camera, renderer.domElement)
   controls.target.set(0, 0.85, 0)
   controls.screenSpacePanning = true
   controls.update()
+  }
 
   // シーンの設定
   const scene = new THREE.Scene()
@@ -174,7 +177,7 @@ window.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < bones.length; i++) {
       boneNode[i] = vrm.humanoid.getBoneNode(bones[i])
     }
-    
+
     NP = <HTMLInputElement>document.getElementById('NPscript');
     ALL_NP = <HTMLInputElement>document.getElementById('ALL_NPscript');
     if (Number(NP.value) > 0 && Number(ALL_NP.value) <= 1) {
