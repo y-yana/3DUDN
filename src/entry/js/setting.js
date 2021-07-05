f5372322c28b523c250da145bfb571a3d48c334e
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-07-05 20:57:30 +0900

    move muteBtn

--- src/entry/js/setting.js
+++ src/entry/js/setting.js
@@ -1,19 +1,15 @@
 document.getElementById('setting').onclick = function () {
   var renameForm = document.getElementById('renameArea');
   var modelUpForm = document.getElementById('modelChangeArea');
-  var muteBtn = document.getElementById('muteBtn');
 
   var renameStyle = renameForm.style.display;
   var modelUpStyle = modelUpForm.style.display;
-  var muteBtnStyle = muteBtn.style.display;
 
-  if (renameStyle == 'none' || modelUpStyle == 'none' || muteBtnStyle == 'none') {
+  if (renameStyle == 'none' || modelUpStyle == 'none') {
     renameForm.style.display = 'block';
     modelUpForm.style.display = 'block';
-    muteBtn.style.display = 'block';
   } else {
     renameForm.style.display = 'none';
     modelUpForm.style.display = 'none';
-    muteBtn.style.display = 'none';
   }
 }
