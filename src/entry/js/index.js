2383c835f2912bb8083bc4142e7ad0bd9ebe3ca3
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-07-05 20:22:19 +0900

    file split

--- src/entry/js/index.js
+++ src/entry/js/index.js
@@ -7,6 +7,7 @@ import '../js/background.js';
 import '../js/setting.js';
 import '../js/enterKey.js';
 import '../js/speechRecognition.js';
+import '../js/download_png.js';
 
 // Sass
 import '../style/index.scss';
